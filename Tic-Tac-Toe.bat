44bc52ad8e0b4d94c67db340411a6fa2c69081d7
AriPope <32315677+AriPope@users.noreply.github.com>
2017-09-26 22:52:44 -0500

    Update 3
    Edited board, added boxes, added call displayText's

--- Tic-Tac-Toe.bat
+++ Tic-Tac-Toe.bat
@@ -1,17 +1,26 @@
 @echo off
 title Tic-Tac-Toe
 color 0F
+set box1=
+set box2=
+set box3=
+set box4=
+set box5=
+set box6=
+set box7=
+set box8=
+set box9=
 :menu
 echo 			=======================
 echo 			Welcome to Tic-Tac-Toe!
 echo 			=======================
-echo ========
-echo 1) Play
-echo ========
-echo 2) Help
-echo ========
-echo 3) Exit
-echo ========
+echo  ========
+echo  1) Play
+echo  ========
+echo  2) Help
+echo  ========
+echo  3) Exit
+echo  ========
 set /p menu= 
 if %menu% equ 1 goto start
 if %menu% equ 2 goto help
@@ -30,70 +39,71 @@ echo.
 echo %~1%
 goto:EOF
 :drawBoard
-pause
 cls
 echo.
 echo.
 echo.
-echo  1 I 2 I 3
-echo ___________
-echo.
-echo  4 I 5 I 6
-echo ___________
-echo.
-echo  7 I 8 I 9
+echo                 %box7%  I %box8%  I %box9%
+echo                -----------
+echo                 %box4%  I %box5%  I %box6%
+echo                -----------
+echo                 %box1%  I %box2%  I %box3%
 echo.
 echo.
-echo Where would you like to move (1-9)?
+echo    Where would you like to move (1-9)?
+echo.
+echo                   7 8 9
+echo                   4 5 6
+echo                   1 2 3
 set /p selection=
 goto:EOF
 :checkForWinner
 goto:EOF
 :start
-call :displayText "Hello player!" & echo. & echo.What is your name?
+call :displayText " Hello player!" & echo. & echo. What is your name?
 echo.
 set /p name=
-call :displayText "Welcome %name%, I am the Game Master"
+call :displayText " Welcome %name%, I am the Game Master" & echo. & echo.
 pause
-:first select
-cls
+:firstSelect
+call :displayText " Please select your gamemode, %name%."
 echo.
-echo Please select your gamemode, %name%.
-echo.
-echo Enter '1' for SINGLE-PLAYER or '2' for TWO-PLAYER
+echo  ==================
+echo   1) Single Player
+echo  ==================
+echo   2) Two Player
+echo  ==================
 echo.
 set /p select=
 if %select% equ 1 goto single
 if %select% equ 2 goto double
-if %select% neq 1 goto first select
+if %select% neq 1 goto firstSelect
 :single
-cls
+call :displayText " Please select your difficulty, %name%."
 echo.
-echo Please select your difficulty, %name%,
-echo.
-echo EASY, MEDIUM, or HARD?
+echo  ===========
+echo   1) Easy
+echo  ===========
+echo   2) Medium
+echo  ===========
+echo   3) Hard
+echo  ===========
 echo.
 set /p difficulty=
-if %difficulty% equ EASY goto play
-if %difficulty% equ MEDIUM goto play
-if %difficulty% equ HARD goto play
-if %difficulty% neq EASY goto first select
+if %difficulty% equ 1 goto play
+if %difficulty% equ 2 goto play
+if %difficulty% equ 3 goto play
+if %difficulty% neq 1 goto single
 :double
-cls
-echo.
-echo What is the second player's name?
+call :displayText " What is the second player's name?"
 echo.
 set /p name2=
 echo.
-cls
-echo.
-echo Welcome to the game, %name2%.
+call :displayText " Welcome to the game, %name2%."
 echo.
 pause
 goto play
 :play
 cls
 call :drawBoard
-pause
-echo Hello
 pause
